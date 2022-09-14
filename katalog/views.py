be4e203200391b8e547268332f77bcfe9fe6bf9e
Kade Satrya <kade.satrya@ui.ac.id>
2022-09-14 21:49:56 +0700

    sampai poin 3 namun isi tabel tidak muncul

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    return render(request, "katalog.html", context)
+
+data_item_katalog = CatalogItem.objects.all()
+context = {
+    "list_item": data_item_katalog,
+    "nama": "Kade Satrya Noto Sadharma",
+    "npm": "2106752376"
+    }
