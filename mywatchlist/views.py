2e1fcf022a54891df9b6d657d037d9094c7abf05
Kade Satrya <kade.satrya@ui.ac.id>
2022-09-22 01:18:10 +0700

    tugas bonus dari tugas 3

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -5,7 +5,7 @@ from django.core import serializers
 
 # Function untuk mywatchlist/
 def show_watchlist_home(request):
-    return render(request, "watchlist_home.html")
+    return render(request, "watchlist_home.html", context)
 
 # Function untuk mywatchlist/html
 def show_watchlist_html(request):
@@ -20,6 +20,22 @@ def show_watchlist_json(request):
     return HttpResponse(serializers.serialize("json", mywatchlist_data), content_type="application/json")
 #Note: request pada fungsi xml dan json tetap harus dimasukkan pada parameter meskipun tak diakses
 mywatchlist_data = MyWatchList.objects.all()
+
+# Tugas bonus
+# Mengubah mywatchlist_data menjadi dictionary
+# lalu mencari jumlah yang sudah ditonton dan belum ditonton
+# Jumlah yang sudah ditonton dicari dengan menjumlahlan semua nilai field "watched"
+# Jumlah yang belum ditonton dicari dengan mengurangi panjang dictionary dengan jumlah yang telah ditonton
+# Lalu kedua nilai tersebut dimasukkan ke context
+mywatchlist_data_dict = mywatchlist_data.values()
+mywatchlist_length = len(mywatchlist_data_dict)
+mywatchlist_watched = 0
+for watchlist in mywatchlist_data_dict:
+    mywatchlist_watched += watchlist["watched"]
+mywatchlist_not_watched = mywatchlist_length - mywatchlist_watched
+
 context={
-    "watchlist_data" : mywatchlist_data
+    "watchlist_data" : mywatchlist_data,
+    "sudah_ditonton": mywatchlist_watched,
+    "belum_ditonton": mywatchlist_not_watched
 }
